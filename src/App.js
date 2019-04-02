95ce9a389803a74f20fce69a38b86bce9398fb0d
zhuxchong <zhuxchong@gmail.com>
2019-04-02 12:47:19 +1100

    lazy demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Suspense } from "react";
 
+function lazyWithPreload(factory) {
+  const Component = React.lazy(factory);
+  Component.preload = factory;
+  return Component;
+}
+const Comp = lazyWithPreload(() => {
+  return import("./component/mycomp");
+});
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div>header</div>
+        <Suspense fallback={<div>loading...</div>}>{<Comp />}</Suspense>
       </div>
     );
   }
---
+++ src/component/mycomp.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MyComp = () => {
+  return <div>lazy loading Component</div>;
+};
+
+export default MyComp;
